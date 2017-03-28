6fccb82d67b156bc04792c742737124fb443147e
Goory al Hamed <gooryalhamed@hotmail.com>
2017-03-28 19:39:00 +0300

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  include Pundit  
+  protect_from_forgery
+  rescue_from Pundit::NotAuthorizedError, with: :access_denied
+  protect_from_forgery with: :exception
+
+
+  private
+
+  def access_denied(exception) 
+    redirect_to users_path, :alert => "Access denied." 
+  end
 end
---
+++ app/policies/note_policy.rb
@@ -0,0 +1,34 @@
+class NotePolicy < ApplicationPolicy
+     attr_reader :note
+ 
+     def initialize(user, note)
+       super(user, note)
+       @note = record
+     end
+ 
+     def show?
+         has_permition?  || user.moderator? ||is_viewer? 
+
+     end
+     def update?
+         has_permition? 
+
+     end
+     def edit?
+         has_permition? 
+     end
+     def destroy?
+      has_permition?
+     end
+     def create?
+      has_permition?
+     end
+     private
+      def has_permition?
+         note.try(:user) == user || user.admin? 
+      end
+      def is_viewer?
+         note.readers.detect{|u|u==user} !=nil ? true : false
+
+      end
+   end
